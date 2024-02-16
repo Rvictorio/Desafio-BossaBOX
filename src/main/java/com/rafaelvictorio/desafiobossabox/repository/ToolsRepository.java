29133650882cd6f275ebce10cfb0481bc42af9a5
rafael <r.victorio@aluno.ufabc.edu.br>
2024-02-16 11:35:54 -0300

    1.2 - Adição das funcionalidades de busca geral e busca por tag.

--- src/main/java/com/rafaelvictorio/desafiobossabox/repository/ToolsRepository.java
+++ src/main/java/com/rafaelvictorio/desafiobossabox/repository/ToolsRepository.java
@@ -3,6 +3,9 @@ package com.rafaelvictorio.desafiobossabox.repository;
 import com.rafaelvictorio.desafiobossabox.entity.Tools;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ToolsRepository extends JpaRepository<Tools, Long> {
 
+    public List<Tools> findByTags(String tags);
 }
--- src/main/java/com/rafaelvictorio/desafiobossabox/services/ToolsService.java
+++ src/main/java/com/rafaelvictorio/desafiobossabox/services/ToolsService.java
@@ -1,7 +1,10 @@
 package com.rafaelvictorio.desafiobossabox.services;
 
+import com.rafaelvictorio.desafiobossabox.entity.Tools;
 import com.rafaelvictorio.desafiobossabox.repository.ToolsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToolsService {
@@ -12,4 +15,15 @@ public class ToolsService {
     public ToolsService(ToolsRepository toolsRepository) {
         this.toolsRepository = toolsRepository;
     }
+
+    public List<Tools> getAllTools(){
+      List<Tools> toolsList =  toolsRepository.findAll();
+      return toolsList;
+    }
+
+    public List<Tools> findToolsTag(String tags){
+       List<Tools> toolsList = toolsRepository.findByTags(tags);
+       return toolsList;
+    }
+
 }
